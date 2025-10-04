e5eb8327a4305f59a9df850cee08ec048b4d95b1
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-10-04 20:01:18 +0530

    modifying the index.jsx

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,18 +1,31 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text style={{color:"red"}}>Edit app/index.tsx to edit this screen hellow world</Text>
+      <Text style={styles.heading}>Edit app/index.tsx to edit this screen hellow world</Text>
       <Link href={"/about"}>About</Link>
+
     </View>
+
+
   );
 }
+
+const styles=StyleSheet.create({
+  container:{
+    flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+       
+  },
+  heading:{
+    fontsize:40,
+    color:"purple",
+  }
+})
