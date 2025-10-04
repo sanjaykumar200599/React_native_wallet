c2f3e93db9bcb5cf194f310e89cfd89d8d09e494
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-10-04 21:09:40 +0530

    Added the styles

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import SafeScreen from "@/components/SafeScreen"
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+           <Slot />
+      </SafeScreen>
+   </ClerkProvider>
+
+  )
 }
+
