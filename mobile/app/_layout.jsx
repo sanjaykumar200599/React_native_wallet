0b854ff67cda12366c4794214f960eda0d864631
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-10-07 23:45:10 +0530

    modified the layout

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,7 +1,7 @@
-import { Stack } from "expo-router";
 import SafeScreen from "@/components/SafeScreen"
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
